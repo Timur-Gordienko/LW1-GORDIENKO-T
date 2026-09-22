bc1b0dd61f1f345fa0b8d1627b520b9fc6129a4b
timur_gord <132463778+Timur-Gordienko@users.noreply.github.com>
2026-09-22 15:53:24 +0300

    Update main10.cpp

--- main10.cpp
+++ main10.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 using namespace std;
-
+//begin10
 int main()
 {
-    // Task: Find the areas of two circles and the ring
+    // Завдання: знайти площі двох кругів і кільця
 
-    // Declaration of variables
+    // Оголошення змінних
     double R1, R2;
     double S1, S2, S3;
     double pi = 3.14;
 
-    // Start of the task
+    // Початок завдання
     cout << "Task: Find the areas of two circles and the ring" << endl;
 
-    // Input of the radii
+    // Введення радіусів
     cout << "Enter outer radius R1: ";
     cin >> R1;
 
     cout << "Enter inner radius R2: ";
     cin >> R2;
 
-    // Calculate the area of the first circle
+    // Обчислити площу першого кола
     S1 = pi * R1 * R1;
 
-    // Calculate the area of the second circle
+    // Обчислити площу другого кола
     S2 = pi * R2 * R2;
 
-    // Calculate the area of the ring
+    // Обчислити площу кільця
     S3 = S1 - S2;
 
-    // Output the results
+    // Вивести результати
     cout << "Area of the first circle S1: " << S1 << endl;
     cout << "Area of the second circle S2: " << S2 << endl;
     cout << "Area of the ring S3: " << S3 << endl;
